2e5876485b50ea7b4a19ea07ecac2a3e8388197b
Koding <rc.chayjocol.alma@leadps.org>
2016-02-24 22:32:00 +0000

    Partner Practice2/24

---
+++ 4-4FunctiontoreadData/walk.py
@@ -0,0 +1,9 @@
+ 
+walking_file = open('walking_instructions.txt' , 'r')
+ 
+lineToPrint = walking_file.readline() 
+while lineToPrint != " ":
+        print(lineToPrint + ", duh") 
+lineToPrint = walking_file.readline()
+ 
+walking_file.close()
